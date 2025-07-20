eefa11954775a94025d3c7ae9b578f29c65dfb29
noootboi <yodo1321@gmail.com>
2025-07-20 19:54:56 +1000

    draw 3

--- keymap_img/update_keymap_img.sh
+++ keymap_img/update_keymap_img.sh
@@ -4,4 +4,4 @@ echo 'Parsing ZMK keymap...'
 keymap -c keymap_config.yaml parse -z ../boards/shields/yk_do52pro/yk_do52pro.keymap > keymap.yaml
 
 echo '\n\nDrawing keymap...'
-keymap -c keymap_config.yaml draw --qmk-keyboard crkbd/rev1 --qmk-layout LAYOUT_split_3x6_3 keymap.yaml > keymap.svg
+keymap -c keymap_config.yaml draw --qmk-keyboard crkbd/rev1 --qmk-layout LAYOUT_split_5x6_3 keymap.yaml > keymap.svg
